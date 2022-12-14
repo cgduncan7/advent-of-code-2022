6d6543daf5b932be4749e1c5969035d43b7d9cad
Collin Duncan <collin@klikkie.com>
2022-12-14 17:16:15 +0100

    cleanup and day 12 part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -41,10 +41,6 @@ impl Node {
     fn can_traverse_to(&self, other: &Node) -> bool {
         self.height + 1 >= other.height
     }
-
-    fn get_h_val(&self, end: &Node) -> usize {
-        end.location.0.abs_diff(self.location.0) + end.location.1.abs_diff(self.location.1)
-    }
 }
 
 fn part1(lines: &mut Lines) -> usize {
@@ -53,7 +49,6 @@ fn part1(lines: &mut Lines) -> usize {
     let mut cols = 0;
 
     let mut start_loc: (usize, usize) = (0, 0);
-    let mut end_loc: (usize, usize) = (0, 0);
 
     for (row, line) in lines.enumerate() {
         if cols == 0 {
@@ -66,10 +61,7 @@ fn part1(lines: &mut Lines) -> usize {
                     start_loc = (row, col);
                     NodeType::Start
                 },
-                'E' => {
-                    end_loc = (row, col);
-                    NodeType::End
-                },
+                'E' => NodeType::End,
                 _ => NodeType::Generic,
             };
             let n = Node::new(row, col, height_char, node_type);
@@ -78,7 +70,6 @@ fn part1(lines: &mut Lines) -> usize {
     }
 
     let start_node_index = start_loc.0 * cols + start_loc.1;
-    let end_node_index = end_loc.0 * cols + end_loc.1;
 
     let mut stack: Vec<(usize, usize)> = Vec::new();
     stack.push((start_node_index, 0));
@@ -87,7 +78,10 @@ fn part1(lines: &mut Lines) -> usize {
 
     while !stack.is_empty() {
         let (current_node_index, depth) = stack.pop().unwrap();
-        let mut neighbor_indices: Vec<usize>;
+        if depth > answer {
+            continue;
+        }
+        let neighbor_indices: Vec<usize>;
         {
             {
                 nodes.borrow_mut()[current_node_index].visited_at = Some(depth);
@@ -124,13 +118,6 @@ fn part1(lines: &mut Lines) -> usize {
                     current_node.can_traverse_to(n) && less_depth
                 })
                 .collect::<Vec<usize>>();
-
-            let end_node = &nodes.borrow()[end_node_index];
-            neighbor_indices.sort_by(|a, b| {
-                let an = &nodes.borrow()[*a];
-                let bn = &nodes.borrow()[*b];
-                bn.get_h_val(end_node).cmp(&an.get_h_val(end_node))
-            });
         }
 
         for ni in neighbor_indices {
@@ -143,12 +130,97 @@ fn part1(lines: &mut Lines) -> usize {
 }
 
 fn part2(lines: &mut Lines) -> usize {
-    0
+    let nodes: RefCell<Vec<Node>> = RefCell::new(Vec::new());
+    let rows = lines.clone().count();
+    let mut cols = 0;
+
+    let mut start_locs: Vec<(usize, usize)> = Vec::new();
+
+    for (row, line) in lines.enumerate() {
+        if cols == 0 {
+            cols = line.len();
+        }
+
+        for (col, height_char) in line.chars().enumerate() {
+            let node_type = match height_char {
+                'S' | 'a' => {
+                    start_locs.push((row, col));
+                    NodeType::Start
+                },
+                'E' => NodeType::End,
+                _ => NodeType::Generic,
+            };
+            let n = Node::new(row, col, height_char, node_type);
+            nodes.borrow_mut().push(n);
+        }
+    }
+
+    
+    let mut stack: Vec<(usize, usize)> = Vec::new();
+
+    for start_loc in start_locs {
+        let start_node_index = start_loc.0 * cols + start_loc.1;
+        stack.push((start_node_index, 0));
+    }
+
+    let mut answer = usize::MAX;
+
+    while !stack.is_empty() {
+        let (current_node_index, depth) = stack.pop().unwrap();
+        if depth > answer {
+            continue;
+        }
+        let neighbor_indices: Vec<usize>;
+        {
+            {
+                nodes.borrow_mut()[current_node_index].visited_at = Some(depth);
+            }
+
+            let current_node = &nodes.borrow()[current_node_index];
+
+            let (current_row, current_col) = current_node.location;
+            if current_node.node_type == NodeType::End {
+                if depth < answer {
+                    answer = depth;
+                }
+                continue;
+            }
+
+            // get possible next steps
+            // let index = current_row * cols + current_col;
+            let neighbor_locs = vec![
+                (current_row.checked_sub(1), Some(current_col)), // top
+                (Some(current_row + 1), Some(current_col)), // bottom
+                (Some(current_row), current_col.checked_sub(1)), // left
+                (Some(current_row), Some(current_col + 1)), // right
+            ];
+
+            // branch into new paths
+            neighbor_indices = neighbor_locs.iter()
+                .filter(|(r, c)| r.is_some() && c.is_some())
+                .map(|(r, c)| (r.unwrap(), c.unwrap()))
+                .filter(|(r, c)| r >= &0 && r < &rows && c >= &0 && c < &cols)
+                .map(|(r, c)| r * cols + c)
+                .filter(|i| {
+                    let n = &nodes.borrow()[*i];
+                    let less_depth = n.visited_at.is_none() || n.visited_at.unwrap() > (depth + 1);
+                    current_node.can_traverse_to(n) && less_depth
+                })
+                .collect::<Vec<usize>>();
+        }
+
+        for ni in neighbor_indices {
+            nodes.borrow_mut()[ni].visited_at = Some(depth);
+            stack.push((ni, depth + 1))
+        }
+    }
+
+    answer
 }
 
 fn main() {
     harness::time_function("./example.txt", &part1);
     harness::time_function("./data.txt", &part1);
-//     harness::time_function("./example.txt", &part2);
-//     harness::time_function("./data.txt", &part2);
+    harness::time_function("./example.txt", &part2);
+    harness::time_function("./data.txt", &part2);
 }
